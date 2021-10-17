2dd724612503002ae0960114678f62feb00f3a94
Giancarlo Messina <giancarlomex@outlook.com>
2021-10-17 10:03:11 +0200

    Add project files.

---
+++ Calcolo_hash/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Calcolo_hash
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variabile di progettazione necessaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Pulire le risorse in uso.
+		/// </summary>
+		/// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Codice generato da Progettazione Windows Form
+
+		/// <summary>
+		/// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+		/// il contenuto del metodo con l'editor di codice.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.buttonInfo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 26);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(204, 87);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Seleziona File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(245, 26);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(385, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(245, 91);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(768, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(117, 217);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "MD5";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(77, 290);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "SHA256";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(189, 222);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(871, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(189, 294);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(871, 22);
+            this.textBox4.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(353, 144);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(421, 52);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Calcola codici HASH";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // buttonInfo
+            // 
+            this.buttonInfo.Location = new System.Drawing.Point(842, 26);
+            this.buttonInfo.Name = "buttonInfo";
+            this.buttonInfo.Size = new System.Drawing.Size(75, 23);
+            this.buttonInfo.TabIndex = 8;
+            this.buttonInfo.Text = "Info";
+            this.buttonInfo.UseVisualStyleBackColor = true;
+            this.buttonInfo.Click += new System.EventHandler(this.buttonInfo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1125, 362);
+            this.Controls.Add(this.buttonInfo);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Calcolatore Hash";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonInfo;
+    }
+}
+
---
+++ Calcolo_hash/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Media;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calcolo_hash
+{
+    public partial class Form1 : Form
+	{
+		private static readonly SHA256 Sha256 = SHA256.Create(); // Istanzio gli oggetti SHA256 e MD5
+		private static readonly MD5 Md5 = MD5.Create();
+		private static string output = string.Empty;
+			 
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog selez_file = new OpenFileDialog();
+
+			if (selez_file.ShowDialog() == DialogResult.OK)
+			{
+				textBox2.Text = selez_file.FileName; // Imposto il solo nome del file selezionato alla prima textbox
+				textBox1.Text = selez_file.SafeFileName; // imposto tutto il percorso!
+			}
+		}
+
+		private void label2_Click(object sender, EventArgs e)
+		{ }
+
+
+		/// <summary>
+		/// Esegui in maniera asincrona il calcolo del Hash SHA256
+		/// </summary>
+		/// <param name="pathCompletaFile"></param>
+		/// <returns></returns>
+		private static async Task CalcolaHashSHA256(string pathCompletaFile)
+		{
+			await Task.Run(() =>
+			{
+				byte[] arrayByte = null;
+				string result = string.Empty;
+				using (FileStream stream = File.OpenRead(pathCompletaFile))
+				{
+					arrayByte = Sha256.ComputeHash(stream);
+				}
+				foreach (byte b in arrayByte)
+				{
+					result += b.ToString("x2");
+				}
+				output = result;
+			});
+		}
+
+
+		/// <summary>
+		/// Esegui in maniera asincrona il calcolo del HASH MD5
+		/// </summary>
+		/// <param name="pathCompletaFile"></param>
+		/// <returns></returns>
+		public static async Task CalcolaHashMD5(string pathCompletaFile)
+		{
+			await Task.Run(() =>
+			{
+				byte[] arrayByte = null; 
+				string result = string.Empty;
+				using (FileStream stream = File.OpenRead(pathCompletaFile))
+				{
+					arrayByte = Md5.ComputeHash(stream);
+				}	
+				foreach (byte b in arrayByte)
+				{
+					result += b.ToString("x2");
+				}
+				output = result;
+			});
+		}
+
+		private async void button2_Click(object sender, EventArgs e)
+		{
+			if (textBox2.Text.ToLower() == "")
+			{
+				System.Windows.Forms.MessageBox.Show("E' necessario selezionare un file!", "ATTENZIONE", MessageBoxButtons.OK);
+			}
+			else
+			{
+				if (File.Exists(textBox2.Text)) // Eseguo un controllo sulla validità del file scelto in input!
+				{
+					Task primoTask = CalcolaHashMD5(textBox2.Text);
+					await primoTask;
+					textBox3.Text = output;
+					Task secondoTask = CalcolaHashSHA256(textBox2.Text);
+					await secondoTask;
+					textBox4.Text = output;
+					SystemSounds.Hand.Play();
+				}
+				else
+				{
+					System.Windows.Forms.MessageBox.Show("Il file selezionato non è valido!", "ERRORE", MessageBoxButtons.OK);
+				}
+			}
+		}
+
+        private void buttonInfo_Click(object sender, EventArgs e)
+        {
+			System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+			System.Diagnostics.FileVersionInfo fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
+			string version = fvi.FileVersion;
+
+			MessageBox.Show($"Versione applicativo: {version}", "Info versione", MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
+    }
+}
